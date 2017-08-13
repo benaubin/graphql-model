d405d910d9f8219be0c513181c775dc96f359b0b
Ben Aubin <ben@bensites.com>
2017-08-13 15:07:10 -0500

    init commit

---
+++ lib/graphql/model.rb
@@ -0,0 +1,7 @@
+require "graphql/model/version"
+
+module Graphql
+  module Model
+    # Your code goes here...
+  end
+end
---
+++ lib/graphql/model/version.rb
@@ -0,0 +1,5 @@
+module Graphql
+  module Model
+    VERSION = "0.1.0"
+  end
+end
